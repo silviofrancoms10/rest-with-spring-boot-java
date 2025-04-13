a3c0eb20de9b84d42ce9a1b246d2ad8cef4ed914
silviofranco <silviofrancoms@gmail.com>
2025-04-12 21:07:46 -0400

    first commit, .gitignore modelo

---
+++ rest-with-spring-boot-java/src/main/java/br/com/silviofrancoms/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.silviofrancoms.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ rest-with-spring-boot-java/src/test/java/br/com/silviofrancoms/RestWithSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.silviofrancoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
