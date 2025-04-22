f156000ef92977830f053546b72e0ac598f372b9
silviofrancoms10 <silviofrancoms@gmail.com>
2025-04-21 20:58:14 -0400

    37 Tratando Exceptions com @ControllerAdvice @ResponseStatus e @ExceptionHandler

--- rest-with-spring-boot-java/src/main/java/br/com/silviofrancoms/controllers/MathController.java
+++ rest-with-spring-boot-java/src/main/java/br/com/silviofrancoms/controllers/MathController.java
@@ -1,5 +1,6 @@
 package br.com.silviofrancoms.controllers;
 
+import br.com.silviofrancoms.exception.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,13 @@ public class MathController {
 
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please insert a numeric value!");
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
 
     }
 
-    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
-        if (strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+    private Double convertToDouble(String strNumber) throws UnsupportedMathOperationException {
+        if (strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please insert a numeric value!");
         String number = strNumber.replace(",", ".");
         return Double.parseDouble(number);
     }
