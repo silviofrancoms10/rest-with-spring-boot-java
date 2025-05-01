5ad5119f81e176cb5fdce873bacc9a4dc79052bf
silviofrancoms10 <silviofrancoms@gmail.com>
2025-05-01 16:58:23 -0400

    Ajustando Exceptions

--- rest-with-spring-boot-java/src/main/java/br/com/silviofrancoms/exception/handler/CustomEntityResponseHandler.java
+++ rest-with-spring-boot-java/src/main/java/br/com/silviofrancoms/exception/handler/CustomEntityResponseHandler.java
@@ -1,12 +1,11 @@
 package br.com.silviofrancoms.exception.handler;
 
 import br.com.silviofrancoms.exception.ExceptionResponse;
-import br.com.silviofrancoms.exception.UnsupportedMathOperationException;
+import br.com.silviofrancoms.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -24,12 +23,12 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request) {
         ExceptionResponse response = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false));
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
