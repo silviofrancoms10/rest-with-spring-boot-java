226f9c2d9028535c36856fae79da899eb28ce404
silviofrancoms10 <silviofrancoms@gmail.com>
2025-05-01 21:15:57 -0400

    Adicionando Perfil dev banco h2 e Perfil prod banco mysql

---
+++ rest-with-spring-boot-java/src/main/java/br/com/silviofrancoms/config/ProdConfig.java
@@ -0,0 +1,9 @@
+package br.com.silviofrancoms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+}
